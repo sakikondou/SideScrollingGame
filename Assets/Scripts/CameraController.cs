f07bf7ffd9a439d04f019f2b04e4d70f8a522af2
TsutayaJun <dominopizza.0913@icloud.com>
2021-08-03 22:01:42 +0900

    追加：カメラがプレイヤーを追従するようにした

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    /// <summary>
+    /// プレイヤーのトランスフォーム
+    /// </summary>
+    [SerializeField] Transform m_playerTrans;
+    [SerializeField] Transform m_stageRSideTrans;
+    [SerializeField] Transform m_stageLSideTrans;
+    private void Update()
+    {
+        if (m_stageLSideTrans.position.x < m_playerTrans.position.x && m_stageRSideTrans.position.x > m_playerTrans.position.x)
+        {
+            transform.position = new Vector3(m_playerTrans.position.x, transform.position.y, transform.position.z);
+        }
+        
+    }
+}
