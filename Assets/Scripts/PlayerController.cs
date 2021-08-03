d73a86e4cb2c1ad07ea1d3f1c231e0986a5f3fdb
TsutayaJun <dominopizza.0913@icloud.com>
2021-08-03 19:59:44 +0900

    追加：横移動できるようにした

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] InputController m_inputController;
+    PlayerInputAction m_inputActions;
+    Rigidbody2D m_rb;
+
+    PlayerMove m_playerMove;
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [SerializeField] float m_moveSpeed = 5f;
+
+    private void Start()
+    {
+        m_inputActions = m_inputController.InputActions;
+        m_rb = GetComponent<Rigidbody2D>();
+
+        m_playerMove = gameObject.AddComponent<PlayerMove>();
+        m_playerMove.Init(m_inputActions, m_rb);
+    }
+
+    private void Update()
+    {
+        m_playerMove.Move(m_moveSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    PlayerInputAction m_inputActions;
+    Rigidbody2D m_rb;
+
+    Vector2 m_inputAxis;
+
+    public void Init(PlayerInputAction inputActions, Rigidbody2D rb)
+    {
+        m_inputActions = inputActions;
+        m_rb = rb;
+    }
+
+    /// <summary>
+    /// 移動する
+    /// </summary>
+    /// <param name="speed">移動速度</param>
+    public void Move(float speed)
+    {
+        m_inputAxis = m_inputActions.Player.Move.ReadValue<Vector2>();
+        
+        //横移動
+        m_rb.velocity = new Vector2(m_inputAxis.x * speed, m_rb.velocity.y);
+    }
+}
